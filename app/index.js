7eba28e78b06879329d9d5a7594bb74296f103f1
toporek3112 <toporkarol@gmail.com>
2020-09-11 16:20:11 +0200

    named volume fix

--- app/index.js
+++ app/index.js
@@ -10,7 +10,7 @@ var knex = require('knex')({
         port: process.env.POSTGRES_PORT,
         user: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
-        database: 'graphql_poc01'
+        database: process.env.POSTGRES_DATABASE
     }
 });
 
